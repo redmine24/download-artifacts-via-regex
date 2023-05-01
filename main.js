531f9327337612dd36f274d41cabc03cbe88c2e1
Dima Nelen <dimanelen@gmail.com>
2023-05-01 11:04:22 +0200

    push action to external(current) repo

---
+++ main.js
@@ -0,0 +1,63 @@
+const core = require('@actions/core');
+const { Octokit } = require('@octokit/core');
+const { paginateRest } = require('@octokit/plugin-paginate-rest');
+const AdmZip = require('adm-zip');
+
+const token = core.getInput("github_token", { required: true })
+const [owner, repo] = core.getInput("repo", { required: true }).split("/")
+const regex = core.getInput("regex", { required: true })
+const path = core.getInput("path", { required: true })
+
+const OctoPag = Octokit.plugin(paginateRest);
+const octokit = new OctoPag({ auth: token });
+
+const apiVersion = '2022-11-28';
+const headers = { 'X-GitHub-Api-Version': apiVersion };
+
+async function main() {
+  try {
+    const artifacts = await listArtifacts(matchesRegexAndNotExpired);
+    core.info(`==> got artifacts: ${artifacts.length} items:`);
+
+    for (const artifact of artifacts) {
+      await downloadArtifact(artifact);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
+
+function matchesRegexAndNotExpired(artifact) {
+  return artifact.name.match(regex) && artifact.expired !== true;
+}
+
+async function listArtifacts(filterFunc) {
+  const artifacts = await octokit.paginate(`GET /repos/${owner}/${repo}/actions/artifacts`, {
+    per_page: 100,
+    headers,
+  });
+
+  artifacts.forEach((artifact) => {
+    core.info(
+      `==> found artifact: id: ${artifact.id} name: ${artifact.name} size: ${artifact.size_in_bytes} branch: ${artifact.workflow_run.head_branch} expired: ${artifact.expired}`
+    );
+  });
+
+  return artifacts.filter(filterFunc);
+}
+
+async function downloadArtifact(artifact) {
+  core.info(
+    ` - download> id: ${artifact.id} name: ${artifact.name} size: ${artifact.size_in_bytes} branch: ${artifact.workflow_run.head_branch} expired: ${artifact.expired}`
+  );
+
+  const artifactZip = await octokit.request(
+    `GET /repos/${owner}/${repo}/actions/artifacts/${artifact.id}/zip`,
+    { headers }
+  );
+
+  const zip = new AdmZip(Buffer.from(artifactZip.data));
+  zip.extractAllTo(`${path}/${artifact.id}`);
+}
